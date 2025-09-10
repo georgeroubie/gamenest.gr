945439ff952368ecb6f68ed81134dc4bbbb4d8c6
George Roubie <george.roubie@gmail.com>
2025-09-10 10:04:10 +0300

    Add sitemap, robots.txt and meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+          entryFileNames: `[name].${env.npm_package_version}.js`,
+          chunkFileNames: `[name].${env.npm_package_version}.js`,
+          assetFileNames: `[name].${env.npm_package_version}.[ext]`,
+        },
+      },
     },
-  },
-})
+    plugins: [
+      react(),
+      tailwindcss(),
+      Sitemap({
+        hostname: env.VITE_PUBLIC_URL,
+        dynamicRoutes: JSON.parse(env.VITE_SITEMAP_URLS || ''),
+        robots: [
+          {
+            userAgent: '*',
+            allow: JSON.parse(env.VITE_ALLOW_ROBOTS_URLS || ''),
+            disallow: JSON.parse(env.VITE_DISALLOW_ROBOTS_URLS || ''),
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
+});
